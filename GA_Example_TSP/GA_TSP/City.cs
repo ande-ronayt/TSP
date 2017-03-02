9e2eb65ce4a3d33f1ea98cc321015606e1c6fd13
Andrew <andrew@xiaobaoonline100.com>
2017-03-02 18:02:58 +0800

    MTSP GA

---
+++ GA_Example_TSP/GA_TSP/City.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_TSP
+{
+    public class City
+    {
+
+        // Member variables
+        public double x { get; private set; }
+        public double y { get; private set; }
+
+        public double id { get; private set; }
+        // ctor
+        public City(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+            this.id = Program.CurId++; 
+        }
+
+        // Functionality
+        public double distanceTo(City c)
+        {
+            if (this.x == c.x && this.y == c.y) return 999;
+            return Math.Sqrt(Math.Pow((c.x - this.x), 2)
+                            + Math.Pow((c.y - this.y), 2));
+        }
+
+        public static City random()
+        {
+            return new City(Program.r.NextDouble(), Program.r.NextDouble());
+        }
+    }
+}
---
+++ GA_Example_TSP/GA_TSP/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_TSP
+{
+    public class Program
+    {
+        public static Random r { get; private set; }
+
+        public static int CurId { get; set; } 
+        
+        static Program(){
+            CurId = 1;
+        }
+
+        public static void Main()
+        {
+            r = new Random();
+
+            Tour dest = Tour.random(Env.numCities);
+            Population p = Population.randomized(dest, Env.popSize);
+
+            int gen = 0;
+            bool better = true;
+
+            while (true /*gen < 100 */ )
+            {
+                if (better)
+                    display(p, gen);
+
+                better = false;
+                double oldFit = p.maxFit;
+
+                p = p.evolve();
+                if (p.maxFit > oldFit)
+                    better = true;
+
+                gen++;
+            }
+        }
+
+        public static void display(Population p, int gen)
+        {
+            Tour best = p.findBest();
+            System.Console.WriteLine("Generation {0}\n" +
+                "Best fitness:  {1}\n" +
+                "Best distance: {2}\n" +
+            "Tour: {3}", gen, best.fitness, best.distance, best.GetPath());
+        }
+    }
+
+    public static class Env
+    {
+        public const double mutRate = 0.03;
+        public const int elitism = 6;
+        public const int popSize = 60;
+        public const int numCities = 20;
+
+        public const int travelers = 3;
+    }
+}
